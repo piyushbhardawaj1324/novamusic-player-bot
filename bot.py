ee1b2c45f31a3494adca84bf5b7f3e9ae48702c0
piyushbhardawaj1324 <piyushbhardawaj1@gmail.com>
2026-03-10 15:29:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import os
 from pyrogram import Client, filters
 from yt_dlp import YoutubeDL
 
-OWNER_ID = 6317313711
-API_ID = 36845222
-API_HASH = "6b145a7819ea1f045e0b89877575530f"
-BOT_TOKEN = "8674697438:AAES7ApBlzXGJl8muEemsq4GYbQIhwxunNk"
+OWNER_ID = int(os.getenv("6317313711"))
+API_ID = int(os.getenv("36845222"))
+API_HASH = os.getenv("6b145a7819ea1f045e0b89877575530f")
+BOT_TOKEN = os.getenv("8674697438:AAES7ApBlzXGJl8muEemsq4GYbQIhwxunNk")
 
 app = Client("musicbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
