99dc4cff14a8a2a8c2bd39ef8819b3a7fcfa7c84
piyushbhardawaj1324 <piyushbhardawaj1@gmail.com>
2026-03-10 13:35:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
 OWNER_ID = 6317313711
-API_ID = 123456
-API_HASH = "API_HASH"
+API_ID = 36845222
+API_HASH = "6b145a7819ea1f045e0b89877575530f"
 BOT_TOKEN = "8674697438:AAES7ApBlzXGJl8muEemsq4GYbQIhwxunNk"
 
 app = Client("musicbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
