64d1a243e94f0bc74fd36835c19bf5b7a878fb93
piyushbhardawaj1324 <piyushbhardawaj1@gmail.com>
2026-03-10 18:31:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+SESSION_STRING = os.getenv("SESSION_STRING") # Required for User Assistant
