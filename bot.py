effc49aa060e1eda1cc6dc65e0794262641c44ef
piyushbhardawaj1324 <piyushbhardawaj1@gmail.com>
2026-03-10 14:04:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from yt_dlp import YoutubeDL
 
 OWNER_ID = 6317313711
 API_ID = 36845222
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_HASH = "6b145a7819ea1f045e0b89877575530f"
+BOT_TOKEN = "8674697438:AAES7ApBlzXGJl8muEemsq4GYbQIhwxunNk"
 
 app = Client("musicbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
